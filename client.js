bdf7900105416f859a1b8579632ad0eb68c4ee5d
Adam Oberle <oberle.adam@gmail.com>
2023-05-02 10:39:39 -0500

    created sumTiles function, created variables, pushed items into empty array

--- client.js
+++ client.js
@@ -10,8 +10,11 @@ console.log('Step 0 complete! client.js loaded!');
 // Write your code here!
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
+let highScore = 29;
+console.log( highScore )
 
 // 2. Make a new empty array called myScrabbleTiles.
+let myScrabbleTiles = [];
 
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
@@ -22,19 +25,54 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
+// pushing created objects into myScrabbleTiles array
+myScrabbleTiles.push({ tile: 'N', score: 1});
+myScrabbleTiles.push({ tile: 'K', score: 5});
+myScrabbleTiles.push({ tile: 'Z', score: 10});
+myScrabbleTiles.push({ tile: 'X', score: 8});
+// pushing multiple objects in one push below
+myScrabbleTiles.push({ tile: 'D', score: 2},{ tile: 'A', score: 1} );
+console.log( ...myScrabbleTiles );
 
 // 4. Remove the last tile from myScrabbleTiles.
-
+// popping off last value - giving lostTile that value with let,
+// can call on that lostTile variable later
+let lostTile = myScrabbleTiles.pop(myScrabbleTiles[myScrabbleTiles.length-1]);
+console.log( myScrabbleTiles);
+// testing out lostTile variable created with pop
+console.log( 'see you later', lostTile );
+// or
+// myScrabbleTiles.pop() - does same thing - auto knocks off last index
+// push to end - give it a value
+// pop off end
+// shift to front - give it a value
+// unshift off front
 
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
+// curly brackets inside parenthesis denotes object being pushed
+myScrabbleTiles.push({ tile: 'F', score: 4});
+console.log( myScrabbleTiles );
 
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
 function sumTiles(anArray) {
-
+  // holds sum of tile object scores
+  let sum = 0;
+  // for loop to iterate over anArray input, and add each score to sum variable
+  for ( i = 0; i < anArray.length; i++){
+    console.log( 'the tile looks like:', anArray[i])
+    sum = sum + anArray[i].score;
+  }
+  console.log( 'sum is:', sum );
 }
+
+sumTiles( myScrabbleTiles );
+
+// for (let anItem of anArray){
+// anItem rather than anArray[i]
+// }
 
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
